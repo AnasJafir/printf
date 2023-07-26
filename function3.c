9fa7f95d3da70168a9e39a63893b5ca280bacd6f
AnasJafir <dr.anas.jafir@gmail.com>
2023-07-26 10:32:51 -0700

    updating: printf function

--- function3.c
+++ function3.c
@@ -1,50 +1,42 @@
 #include "main.h"
 /**
- * convert_size_number - Casts a number to the specified size
+ * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
-long int convert_size_number(long int num, int size)
+long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
-		return ((short)num);
+		return ((unsigned short)num);
 
-	return ((int)num);
+	return ((unsigned int)num);
 }
 /**
- * print_int - Print int
+ * print_unsigned - Prints an unsigned number
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width.
+ * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_int(va_list types, char buffer[], int flags,
+int print_unsigned(va_list types, char buffer[], int flags,
 		int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	int is_negative = 0;
-	long int n = va_arg(types, long int);
-	unsigned long int num;
+	unsigned long int num = va_arg(types, unsigned long int);
 
-	n = convert_size_number(n, size);
-	if (n == 0)
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
-
-	if (n < 0)
-	{
-		num = (unsigned long int)((-1) * n);
-		is_negative = 1;
-	}
 
 	while (num > 0)
 	{
@@ -53,5 +45,6 @@ int print_int(va_list types, char buffer[], int flags,
 	}
 
 	i++;
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+
+	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
---
+++ function7.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+/**
+ * print_non_printable - Prints ascii codes in hexa of non printable chars
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_non_printable(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	int i = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[i] != '\0')
+	{
+		if (is_printable(str[i]))
+			buffer[i + offset] = str[i];
+		else
+			offset += append_hexa_code(str[i], buffer, i + offset);
+
+		i++;
+	}
+
+	buffer[i + offset] = '\0';
+
+	return (write(1, buffer, i + offset));
+}
+
